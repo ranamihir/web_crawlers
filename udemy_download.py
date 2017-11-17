fde9bd48c435d34c9bb885cdc479a3bb8ca83b44
Mihir Rana <ranamihir@gmail.com>
2017-11-16 19:56:39 -0500

    Redaction.

--- udemy_download.py
+++ udemy_download.py
@@ -62,9 +62,9 @@ browser = webdriver.Chrome('C:/Users/ranamihir/AppData/Local/Programs/Python/Pyt
 url = 'https://citigroup.udemy.com/'
 browser.get(url)
 username = browser.find_element_by_name('USER')
-username.send_keys('as48677')
+username.send_keys('<username>')
 password = browser.find_element_by_name('PASSWORD')
-password.send_keys('c03u5thermo')
+password.send_keys('<password>')
 submit = browser.find_element_by_class_name('ButtonSm')
 submit.click()
 
